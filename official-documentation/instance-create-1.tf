b66ab43aff3653557a3f50a388952f8571a00203
saifali123 <sayyedsaif869@gmail.com>
2020-09-24 00:54:11 +0100

    terraform-official-1

--- official-documentation/instance-create-1.tf
+++ official-documentation/instance-create-1.tf
@@ -16,5 +16,5 @@ resource "aws_instance" "example" {
   ami           = "ami-06fd8a495a537da8b"
   instance_type = "t2.micro"
   subnet_id = "subnet-23cc5b79"
-  vpc_security_group_ids = [sg-0aea740d7f15597de]
+  vpc_security_group_ids = ["sg-0aea740d7f15597de"]
 }
