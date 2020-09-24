f73ff4aee68dcc3243f408fb83068ac5ca64cd13
saifali123 <sayyedsaif869@gmail.com>
2020-09-24 01:49:08 +0100

    variables-1

--- official-documentation/instance-create-1.tf
+++ official-documentation/instance-create-1.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "var.region"
+  region  = var.region
 }
 
 resource "aws_instance" "example" {
--- official-documentation/variables.tf
+++ official-documentation/variables.tf
@@ -4,7 +4,7 @@ variable "region" {
 }
 
 variable "amis" {
-  type = "map"
+  type = map(any)
   default = {
     "eu-west-1" = "ami-08a2aed6e0a6f9c7d"
     "us-west-2" = "ami-0841edc20334f9287"
