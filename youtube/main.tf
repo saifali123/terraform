de862d933cecacfd65733246c82e158a2e550ea9
saifali123 <sayyedsaif869@gmail.com>
2020-10-06 00:41:08 +0100

    youtube-1

---
+++ youtube/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "eu-west-1"
+  profile = "default"
+}
+
+resource "aws_instance" "terraform-naming-instance" {
+  ami = "ami-06fd8a495a537da8b"
+  instance_type = "t3.micro"
+
+}
