3c2bc39484232b6bae964ba5c36050624c34b194
saifali123 <sayyedsaif869@gmail.com>
2020-10-08 00:58:44 +0100

    attributes2

--- hashicorp-terraform(zealvora)/chapter-4(Read,Generate,ModifyConfigurations)/attributes.tf
+++ hashicorp-terraform(zealvora)/chapter-4(Read,Generate,ModifyConfigurations)/attributes.tf
@@ -7,7 +7,14 @@ resource "aws_eip" "terraform-eip" {
   vpc = true
 }
 
+output "eip" {
+  value = aws_eip.terraform-eip.public_ip
+}
+
 resource "aws_s3_bucket" "terraform-bucket" {
   bucket = "saifbucket0038529"
 }
 
+output "s3" {
+  value = aws_s3_bucket.terraform-bucket.bucket_domain_name
+}
