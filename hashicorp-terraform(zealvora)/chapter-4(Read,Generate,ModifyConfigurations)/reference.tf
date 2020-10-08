7c073f85e826334e52b1abd569004621d8f15dc7
saifali123 <sayyedsaif869@gmail.com>
2020-10-08 01:15:30 +0100

    attributes3

--- hashicorp-terraform(zealvora)/chapter-4(Read,Generate,ModifyConfigurations)/reference.tf
+++ hashicorp-terraform(zealvora)/chapter-4(Read,Generate,ModifyConfigurations)/reference.tf
@@ -5,5 +5,9 @@ resource "aws_eip" "terraformeip" {
 resource "aws_instance" "terraformec2" {
   ami = "ami-08a2aed6e0a6f9c7d"
   instance_type = "t2.micro"
-  associate_public_ip_address = aws_eip.terraform-eip.public_ip
+}
+
+resource "aws_eip_association" "terraformeipassoction" {
+  instance_id = aws_instance.terraformec2.id
+  allocation_id = aws_eip.terraform-eip.id
 }
